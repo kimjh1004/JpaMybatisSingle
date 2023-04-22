86f03380838d2b6b8ab97d6275ce1d3eabc91b6c
kimjh1004 <special-10@hanmail.net>
2023-04-23 00:06:26 +0900

    부트스트랩 적용 준비

--- src/main/java/com/boot/controller/SampleController.java
+++ src/main/java/com/boot/controller/SampleController.java
@@ -2,21 +2,35 @@ package com.boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.boot.service.SampleService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class SampleController {
 	
 	@Autowired
 	SampleService service;
 	
-	@RequestMapping("/test")
-	@ResponseBody
+	@GetMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+ 
+	@GetMapping("/test")
 	public String test() {
-		service.sampleService();
-		return "abc";
+		//service.sampleService();
+		return "test";
 	}
+	
+	
+	@GetMapping({"/sample/exLayout1", "/sample/exLayout2","/sample/exTemplate","/sample/exSidebar"})
+    public void exLayout1() {
+        log.info("exLayout1...");
+    }
 }
